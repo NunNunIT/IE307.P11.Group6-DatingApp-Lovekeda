595e99d72bb2b911d8e154efd5e19bbbf7a1fed7
fredle09 <le197903@gmail.com>
2024-12-27 17:55:42 +0700

    fix: fix package.json

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,28 @@
-// module.exports = function (api) {
-//   api.cache(true);
-//   const plugins = [];
-
-//   plugins.push('react-native-reanimated/plugin');
-
-//   return {
-//     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
-//     plugins,
-//   };
-// };
-
 module.exports = function (api) {
   api.cache(true);
-  
+  const plugins = [];
+
+  plugins.push('react-native-reanimated/plugin');
+
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
-    ],
-    plugins: [
-      ['react-native-reanimated/plugin'], // Đặt plugin này cuối cùng
-      ['module:react-native-dotenv']
-    ],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+
+    plugins,
   };
 };
+
+// module.exports = function (api) {
+//   api.cache(true);
+  
+//   return {
+//     presets: [
+//       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+//       'nativewind/babel',
+//     ],
+//     plugins: [
+//       ['react-native-reanimated/plugin'], // Đặt plugin này cuối cùng
+//       ['module:react-native-dotenv']
+//     ],
+//   };
+// };
 
--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,27 @@
-// const { getDefaultConfig } = require("expo/metro-config");
-// const { withNativeWind } = require("nativewind/metro");
-
-// const config = getDefaultConfig(__dirname);
-
-// module.exports = withNativeWind(config, { input: "./global.css" });
-
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-// Lấy cấu hình mặc định
+// Get the default configuration from Expo
 const defaultConfig = getDefaultConfig(__dirname);
 
-// Kết hợp cấu hình NativeWind
+// Integrate NativeWind with additional configuration
 const configWithNativeWind = withNativeWind(defaultConfig, { input: "./global.css" });
 
-// Bổ sung thêm phần mở rộng cho `resolver`
-configWithNativeWind.resolver.sourceExts.push('cjs');
+// Add custom resolver extensions if necessary
+configWithNativeWind.resolver.sourceExts.push("cjs");
 
 module.exports = configWithNativeWind;
+
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require("nativewind/metro");
+
+// // Lấy cấu hình mặc định
+// const defaultConfig = getDefaultConfig(__dirname);
+
+// // Kết hợp cấu hình NativeWind
+// const configWithNativeWind = withNativeWind(defaultConfig, { input: "./global.css" });
+
+// // Bổ sung thêm phần mở rộng cho `resolver`
+// configWithNativeWind.resolver.sourceExts.push('cjs');
+
+// module.exports = configWithNativeWind;
