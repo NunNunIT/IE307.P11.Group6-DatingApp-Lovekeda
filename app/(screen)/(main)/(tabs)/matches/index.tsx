d49961f34c8b2a62f885691890b24c7d3533c9f3
fredle09 <le197903@gmail.com>
2024-12-28 10:36:53 +0700

    feat: fetch and merge user likes with profiles for matches display

--- app/(screen)/(main)/(tabs)/matches/index.tsx
+++ app/(screen)/(main)/(tabs)/matches/index.tsx
@@ -3,19 +3,63 @@ import { Pressable, ScrollView, View } from "react-native";
 import { router } from "expo-router";
 import { DATE_DATA } from "@/constant";
 import { useAuth } from "@/provider/AuthProvider";
+import { useEffect, useState } from "react";
+import { supabase } from "@/utils/supabase";
+import Spinner from "react-native-loading-spinner-overlay";
 
 const db = DATE_DATA;
 
 export default function MatchesScreen1() {
   const { session } = useAuth();
+  const [data, setData] = useState<any[] | undefined>(undefined);
 
-  // Filter out the current user
-  const filteredData = db.filter((item) => item.id !== session?.user?.id);
+  useEffect(() => {
+    (async () => {
+      const [
+        { data: likes, error: errorLikes },
+        { data: profiles, error: errorProfile }
+      ] = await Promise.all([
+        supabase
+          .from('likes')
+          .select("*"),
+        supabase
+          .from('profiles')
+          .select("*")
+      ])
+
+      if (errorLikes || errorProfile) {
+        return;
+      }
+
+      // Filter and merge profiles based on symmetric `user_id` and `target_user_id`
+      const uniquePairs = new Set<string>();
+
+      likes.forEach(like => {
+        const key =
+          like?.user_id && like?.target_user_id && like.user_id < like.target_user_id
+            ? `${like.user_id}-${like.target_user_id}`
+            : `${like.target_user_id}-${like.user_id}`;
+        uniquePairs.add(key);
+      });
+
+      const mergedProfiles = Array.from(uniquePairs).map((pair: string) => {
+        const [user_id, target_user_id] = pair.split("-");
+        const profile = profiles.find(
+          p => p.user_id === user_id || p.user_id === target_user_id
+        );
+        return profile;
+      }).filter(Boolean); // Filter out null values
+
+      setData(mergedProfiles);
+
+    })()
+  });
 
   return (
     <ScrollView className="flex-1 h-full bg-white dark:bg-black">
       <View className="flex flex-row flex-wrap gap-2 justify-between p-2 pb-20">
-        {filteredData.map((item) => (
+        {!data && <Spinner visible={true} />}
+        {data?.map((item) => (
           <Pressable
             key={item.id} // Use `id` for unique keys
             onPress={() => router.push(`/chatDetail/${item.id}`)}
