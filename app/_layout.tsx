981b1d157b24ae3d4171239fa674e85892deba9c
fredle09 <le197903@gmail.com>
2025-01-27 05:12:45 +0700

    feat: add SWRProvider for improved data fetching and state management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,22 +8,25 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { AuthProvider } from "@/providers/AuthProvider";
 import { DumpProvider } from "@/providers/DumpProvider";
+import SWRProvider from "@/providers/SWRProvider";
 
-export { ErrorBoundary} from "expo-router";
+export { ErrorBoundary } from "expo-router";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
-    <DumpProvider>
-      <GestureHandlerRootView className="flex flex-1">
-        <ThemeProvider>
-          <AuthProvider>
-            <Slot />
-            <PortalHost />
-          </AuthProvider>
-        </ThemeProvider>
-      </GestureHandlerRootView>
-    </DumpProvider>
+    <SWRProvider>
+      <DumpProvider>
+        <GestureHandlerRootView className="flex flex-1">
+          <ThemeProvider>
+            <AuthProvider>
+              <Slot />
+              <PortalHost />
+            </AuthProvider>
+          </ThemeProvider>
+        </GestureHandlerRootView>
+      </DumpProvider>
+    </SWRProvider>
   );
 }
